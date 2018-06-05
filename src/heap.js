2cc6454df38071bcee801108b44a0f65f3d8af48
Eric Gomez <eric_gomez_1@hotmail.com>
2018-06-05 15:07:45 -0700

    heap is now complete and passing all tests and we made get max run in constant time as specified.

--- src/heap.js
+++ src/heap.js
@@ -4,29 +4,33 @@ class Heap {
     this.storage = [];
   }
 
+
+  // 2 * i + 1 / 2
   /* Insert the given value into the heap.
-  The heap should maintain the heap property 
+  The heap should maintain the heap property
   after insertion */
   insert(value) {
-
+    this.storage.push(value);
+    this.bubbleUp(this.storage.length - 1);
   }
 
   /* Remove the maximal value from the heap and
   return it. The heap should maintain the heap
   property after removing the maximal value */
   delete() {
-
+    this.siftDown(0);
+    return this.storage.shift();
   }
 
   /* Return the maximal value in the heap
   without removing it */
   getMax() {
-
+    return this.storage[0];
   }
 
   /* Return the size of the heap */
   getSize() {
-
+    return this.storage.length;
   }
 
   /* Moves the element at the specified index "up"
@@ -34,7 +38,11 @@ class Heap {
   parent value is less than the value located at
   the input index */
   bubbleUp(index) {
-
+      const parent = Math.floor((index - 1) / 2);
+      if (this.storage[parent] < this.storage[index]) {
+        [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]]
+        this.bubbleUp(parent);
+      }
   }
 
   /* Move the element at the specified index "down"
@@ -42,7 +50,8 @@ class Heap {
   child's value is greater than the value located at
   the input index */
   siftDown(index) {
-    
+    const maxChild = this.storage[index * 2 + 2] > this.storage[index * 2 + 1] ? index * 2 + 2 : index * 2 + 1;
+    if (this.storage[index] < this.storage[maxChild]) [this.storage[index], this.storage[maxChild]] = [this.storage[maxChild], this.storage[index]], this.siftDown(maxChild);
   }
 }
 
